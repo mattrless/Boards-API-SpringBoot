9f3e77573f065bb6f40cce7c1d8856901138278c
mattrless <carlosdanstd@gmail.com>
2026-03-26 13:41:04 -0600

    initialize project

---
+++ src/main/java/com/boards/api/BoardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.boards.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardsApiApplication.class, args);
+	}
+
+}

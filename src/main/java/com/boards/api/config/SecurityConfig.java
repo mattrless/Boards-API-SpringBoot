2decfc9c96ae0e7d72b6e336123d05eda1b28803
mattrless <carlosdanstd@gmail.com>
2026-04-14 19:34:07 -0600

    add vscode configurations and update path for security configuration

--- src/main/java/com/boards/api/config/SecurityConfig.java
+++ src/main/java/com/boards/api/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.boards.api.security;
+package com.boards.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.boards.api.security.JwtFilter;
 
 import lombok.RequiredArgsConstructor;
 
